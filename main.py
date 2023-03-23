006ccb6cd46f665890fba90239e468882199e1ee
Jesus <pelida@outlook.com>
2023-03-23 09:30:04 -0600

    varias contraseñas

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import paramiko
 host = '169.254.1.1'
 port = 22
 username = 'admin'
-password = '=;.9cTgm;:R\\T9x\\'
+passwords = ['bVmksnIlX4', '=;.9cTgm;:R\\T9x\\', 'x^UftfyJYbb`zk6h']
 
 # Define the commands to reset the RF device
 reset_commands = [
@@ -12,16 +12,30 @@ reset_commands = [
     'reboot'
 ]
 
-# Create an SSH client and connect to the remote server
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(host, port, username, password)
+# Try each password until we successfully connect to the device
+for password in passwords:
+    try:
+        # Create an SSH client and connect to the remote server
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(host, port, username, password)
 
-# Execute the reset commands
-for cmd in reset_commands:
-    stdin, stdout, stderr = ssh.exec_command(cmd)
-    # Wait for the command to complete
-    stdout.channel.recv_exit_status()
+        # Execute the reset commands
+        for cmd in reset_commands:
+            stdin, stdout, stderr = ssh.exec_command(cmd)
+            # Wait for the command to complete
+            stdout.channel.recv_exit_status()
 
-# Close the SSH connection
-ssh.close()
+        # Close the SSH connection
+        ssh.close()
+
+        # If we successfully connected to the device, break out of the loop
+        break
+
+    except paramiko.AuthenticationException:
+        # If the password is incorrect, print an error message and try the next password
+        print(f"Failed to connect to {host} with password {password}")
+
+else:
+    # If we have tried all passwords and still couldn't connect to the device, print an error message
+    print(f"Failed to connect to {host} with any of the provided passwords")
